3e178a2d9da2aa767c2f6941153f00fc520d68ad
Clément Merlin <clement.merlin@shopopop.com>
2020-04-01 21:17:42 +0200

    [feature] UPDATE error handling - ADD Sentry errorflow - UPDATE ApiError class - ADD route "doable" - UPDATE controller - UPDATE user model in DAO

--- CustomError.ts
+++ CustomError.ts
@@ -1,9 +1,15 @@
 export default class ApiError extends Error {
     public statusCode: number;
     public message: string;
+    public code: string;
+    public level: string;
+    public data?;
 
-    constructor(statusCode: number, message: string) {
+    constructor(statusCode: number, message: string, code: string, level: string, data?) {
         super(message);
         this.statusCode = statusCode;
+        this.code = code;
+        this.level = level;
+        this.data = data;
     }
   }
--- dao.ts
+++ dao.ts
@@ -1,13 +1,14 @@
 import ApiError from "./CustomError";
 
-export default async function getUserById(userId: number): Promise<{ name: string; age: number }> {
+export default async function getUserById(userId: number): Promise<{ id: number, name: string, age: number }> {
     return new Promise((resolve, reject) => {
         if (!userId || userId === 0) {
-            const err = new ApiError(404, "User not found");
+            const err = new ApiError(404, "User not found", "USER_NOT_FOUND", "INFO");
             reject(err);
         }
         else {
             resolve({
+                id: userId,
                 name: "John Doe",
                 age: 30
             })
