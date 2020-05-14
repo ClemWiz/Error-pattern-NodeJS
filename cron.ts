255cc758049b440fc2b90fc2637a1e354b8edbfd
Clément Merlin <clement.merlin@shopopop.com>
2020-05-14 14:59:44 +0200

    [refactor] CLEAN cron

--- cron.ts
+++ cron.ts
@@ -1,17 +1,13 @@
 import { getUserById } from "./dao";
 import log from "./logger";
-import ApiError from "./CustomError";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function cron() {
     return setInterval(
         async () => {
-            timed("1", 1000).then(res => console.log(res));
-            console.log(await getUserById(1));
             try {
-                // const user = await getUserById(0);
-                // console.log(user);
-                console.log(await timed("2", 2000));
+                const user = await getUserById(0);
+                console.log(user);
             } catch (error) {
                 log(error);
                 console.error(error);
@@ -19,11 +15,4 @@ export default function cron() {
         },
         10000
     );
-}
-async function timed(msg: string, timer:number) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(msg);
-        }, timer);
-    });
 }
