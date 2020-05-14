5f98937902f8cac103a909a1b8167050094ebca3
Clément Merlin <clement.merlin@shopopop.com>
2020-05-14 14:53:31 +0200

    DELETE unneeded try/catch

--- ctrl.ts
+++ ctrl.ts
@@ -1,60 +1,40 @@
 import { Request, Response } from "express";
 import { getUser} from "./logic";
-import ApiError from "./CustomError";
+import ApiError, { ErrorLevel } from "./CustomError";
 
 export async function doSomethingWithUser(req: Request, res: Response): Promise<void> {
-    // eslint-disable-next-line no-useless-catch
-    try {
         const userId = checkUserId(req.params.id);
         const user = await getUser(userId);
         res.status(200).json(user);
-    }
-    catch (error) {
-        // I can process the error here, or throw it to a generic error catcher (like a middleware, see app.ts)
-        throw error;
-    }
 }
 
 export async function userDoSomething(req: Request, res: Response): Promise<void> {
-    // eslint-disable-next-line no-useless-catch
-    try {
         const userId = checkUserId(req.params.id);
         const user = await getUser(userId);
         const doable = req.params.doable;
 
         switch (doable) {
             case "illegal":
-                throw new ApiError(403, "Illegal action", "ILLEGAL_ACTION", "WARNING", { user, context: { do: doable }});
+                throw new ApiError(403, "Illegal action", "ILLEGAL_ACTION", ErrorLevel.Warning, { user, type: "userAction", context: { do: doable }});
 
             case "error":
-                throw new ApiError(500, "Normal error", "INTERNAL_ERROR", "ERROR", { context: { userId: req.params.id }});
+                throw new ApiError(500, "Normal error", "INTERNAL_ERROR", ErrorLevel.Error, { type: "errorLevel", context: { userId: req.params.id }});
                 
             case "fatal":
-                throw new ApiError(500, "Fatal error", "INTERNAL_ERROR", "FATAL", { context: { userId: req.params.id }});
+                throw new ApiError(500, "Fatal error", "INTERNAL_ERROR", ErrorLevel.Fatal, { type: "errorLevel", context: { userId: req.params.id }});
                 
             default:
                 break;
         }
 
         res.status(200).json({ ...user, do: doable });
-    }
-    catch (error) {
-        // I can process the error here, or throw it to a generic error catcher (like a middleware, see app.ts)
-        throw error;
-    }
 }
 
 function checkUserId(userId: string): number {
-    // eslint-disable-next-line no-useless-catch
-    try {
         const parsedId = parseInt(userId, 10);
     
         if (isNaN(parsedId)) {
-            throw new ApiError(400, "Invalid user ID", "INVALID_USER_ID", "WARNING", { context: { userId }});
+            throw new ApiError(400, "Invalid user ID", "INVALID_USER_ID", ErrorLevel.Warning, { type: "User", context: { userId }});
         }
         else return parsedId;
-    }
-    catch (err) {
-        throw err;
-    }
 }
--- logic.ts
+++ logic.ts
@@ -2,13 +2,7 @@ import { getUserById } from "./dao";
 import { User } from "./models";
 
 export async function getUser(id: number): Promise<User> {
-    // eslint-disable-next-line no-useless-catch
-    try {
         const user = await getUserById(id);
         // do some logic with user here
         return user;
-    }
-    catch (error) {
-        throw error;
-    }
 }
